f5b9b0c58350a31a0b49b0b3ff7309fd16e48a36
chaf666 <144238017+chaf666@users.noreply.github.com>
2024-01-27 02:44:52 +0300

    Add files via upload

---
+++ L4_2task5.pas
@@ -0,0 +1,16 @@
+﻿Program L4_2task5;
+uses graphABC;
+var r,x,y,i:integer;
+begin
+  r:=10;
+  y:=30;
+  x:=50;
+  for i:=0 to 7 do
+  begin
+   Circle(x,y,r);
+   setpencolor(RGB(random(255),random(255),random(255)));
+   r:=r+12;
+   x:=x+70;
+   y:=y+50
+   end;
+end.
